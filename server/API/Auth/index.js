cb604f7373bef4c15909a93ed84aa8202b86e0f6
HarshilDale <harshil125.dale@gmail.com>
2021-09-13 20:52:24 +0530

    Google Auth API

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -2,9 +2,11 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import passport from "passport";
 
 // Models
 import { UserModel } from "../../database/user";
+
 
 const Router = express.Router();
 
@@ -50,4 +52,39 @@ Router.post("/signin", async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 });
+
+/*
+Route   /google
+Des     Google Signin
+Params  none
+Access  Public
+Method  GET
+*/
+Router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
+  })
+);
+
+/*
+Route     /google/callback
+Des       Google Signin Callback
+Params    none
+Access    Public
+Method    GET  
+*/
+Router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    return res.redirect(
+      `http://localhost:3000/google/${req.session.passport.user.token}`
+    );
+  }
+);
+
 export default Router;
--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ require("dotenv").config();
 import express from "express";
 import cors from "cors"; // cross-origin server
 import helmet from "helmet"; // for security
+import passport from "passport";
+
+// Configs
+import googleAuthConfig from "./config/google.config";
 
 // microservice routes
 import Auth from "./API/Auth";
@@ -14,21 +18,27 @@ import ConnectDB from "./database/connection";
 
 const zomato = express();
 
+console.log(process.env);
+
 // Application middlewares
 zomato.use(express.json());
 zomato.use(express.urlencoded({ extended: false }));
 zomato.use(helmet());
 zomato.use(cors());
+zomato.use(passport.initialize());
+zomato.use(passport.session());
+
+// Passport Configuration
+googleAuthConfig(passport);
 
 // Application Routes
 zomato.use("/auth", Auth);
 
 zomato.get("/", (req, res) => res.json({ message: "Setup Success" }));
 
+console.log(process.env.GOOGLE_CLIENT_ID);
 zomato.listen(4000, () =>
   ConnectDB()
     .then(() => console.log("Server is running🚀"))
-    .catch(() =>
-      console.log("Server is running, but DB connection failed...")
-    )
+    .catch(() => console.log("Server is running, but DB connection failed..."))
 );
