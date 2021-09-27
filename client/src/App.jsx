a0500250fb24a32e967905571f39519a214df215
HarshilDale <harshil125.dale@gmail.com>
2021-09-27 21:11:07 +0530

    add navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+// HOC
+import HomeLayoutHOC from "./HOC/Home.HOC";
+
+// Component
+import Temp from "./Components/temp";
+
+function App() {
+  return (
+  <>
+  <HomeLayoutHOC path="/" exact component={Temp} />
+  </>
+  );
+}
+
+export default App;
