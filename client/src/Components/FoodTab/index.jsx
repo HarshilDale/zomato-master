f7d3e3d6cedaeca26426607cbe3fbd494e4ccc65
HarshilDale <harshil125.dale@gmail.com>
2021-09-28 16:52:55 +0530

    add active tab to mobile as well as tab screen

--- client/src/Components/FoodTab/index.jsx
+++ client/src/Components/FoodTab/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams,Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { RiShoppingBag3Line } from "react-icons/ri";
 import { IoFastFoodOutline, IoNutritionOutline } from "react-icons/io5";
 import { BiDrink } from "react-icons/bi";
@@ -10,51 +10,46 @@ const MobileTabs = () => {
       id: "delivery",
       icon: <RiShoppingBag3Line />,
       name: "Delivery",
-      isActive: false,
     },
     {
       id: "dining",
       icon: <IoFastFoodOutline />,
       name: "Dining Out",
-      isActive: false,
     },
     {
       id: "night",
       icon: <BiDrink />,
       name: "Night life",
-      isActive: false,
     },
     {
       id: "nutri",
       icon: <IoNutritionOutline />,
       name: "Nutrition",
-      isActive: false,
     },
   ]);
   const { type } = useParams();
-
 
   return (
     <>
       <div className="lg:hidden bg-white shadow-lg p-3 fixed bottom-0 z-10 w-full flex items-center justify-between text-gray-500 border md:justify-evenly">
         {allTypes.map((items) => (
           <Link to={`/${items.id}`}>
-          <div
-            className={
-              type === items.id
-                ? "flex flex-col relative items-center text-xl text-zomato-400"
-                : "flex flex-col items-center text-xl"
-            }
-          >
             <div
               className={
-                type === items.id &&
-                "absolute -top-3 w-full h-2 border-t-2 border-zomato-400"
+                type === items.id
+                  ? "flex flex-col relative items-center text-xl text-zomato-400"
+                  : "flex flex-col items-center text-xl"
               }
-            />
-            {items.icon}
-            <h5 className="text-sm">{items.name}</h5>
-          </div>
+            >
+              <div
+                className={
+                  type === items.id &&
+                  "absolute -top-3 w-full h-2 border-t-2 border-zomato-400"
+                }
+              />
+              {items.icon}
+              <h5 className="text-sm">{items.name}</h5>
+            </div>
           </Link>
         ))}
       </div>
