f0018e0a96988b3144c4c7fe1d1002cd8e25b5df
HarshilDale <harshil125.dale@gmail.com>
2021-10-01 12:36:39 +0530

    add delivery section

--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -1,13 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 // components
 import DeliveryCarousal from "./DeliveryCarousal";
-
+import Brand from "./Brand";
+import RestaurantCard from "../RestaurantCard";
 
 const Delivery = () => {
+  const [restaurantList, setRestaurantList] = useState([
+    {
+      _id: "123456",
+      photos: [
+        "https://b.zmtcdn.com/data/pictures/chains/9/1401979/133a4b906f9704eb6122caab14f96064_o2_featured_v2.jpg?output-format=webp",
+      ],
+      name: "Shree Gurukripa",
+      cuisine: ["Street Food", "Beverages", "Tea"],
+      averageCost: 100,
+      isPro: true,
+      isOff: 80,
+      durationOfdelivery: 47,
+      restaurantReviewValue:4.1,
+    },
+    {
+      _id: "123456",
+      photos: [
+        "https://b.zmtcdn.com/data/pictures/chains/9/1401979/133a4b906f9704eb6122caab14f96064_o2_featured_v2.jpg?output-format=webp",
+      ],
+      name: "Shree Gurukripa",
+      cuisine: ["Street Food", "Beverages", "Tea"],
+      averageCost: 100,
+      isPro: true,
+      isOff: 80,
+      durationOfdelivery: 47,
+      restaurantReviewValue:4.1,
+    },
+    {
+      _id: "123456",
+      photos: [
+        "https://b.zmtcdn.com/data/pictures/chains/9/1401979/133a4b906f9704eb6122caab14f96064_o2_featured_v2.jpg?output-format=webp",
+      ],
+      name: "Shree Gurukripa",
+      cuisine: ["Street Food", "Beverages", "Tea"],
+      averageCost: 100,
+      isPro: true,
+      isOff: 80,
+      durationOfdelivery: 47,
+      restaurantReviewValue:4.1,
+    },
+  ]);
   return (
     <>
       <DeliveryCarousal />
+      {/*<Brand />*/}
+      <div className="flex justify-between flex-wrap">
+        {restaurantList.map((restaurant) => (
+          <RestaurantCard {...restaurant} key={restaurant._id} />
+        ))}
+      </div>
     </>
   );
 };
