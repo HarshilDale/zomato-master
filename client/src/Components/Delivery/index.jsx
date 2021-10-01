81bb73e555b5b480e66f5e14d0534205b517c97e
HarshilDale <harshil125.dale@gmail.com>
2021-10-01 17:57:13 +0530

    fix redirect

--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -50,10 +50,17 @@ const Delivery = () => {
   return (
     <>
       <DeliveryCarousal />
-      {/*<Brand />*/}
+      {/* <Brand /> */}
+      <h1 className="text-xl mt-4 mb-2 md:mt-8 md:text-3xl md:font-semibold">
+        Delivery Restaurants in Vijay Nagar
+      </h1>
       <div className="flex justify-between flex-wrap">
         {restaurantList.map((restaurant) => (
-          <RestaurantCard {...restaurant} key={restaurant._id} />
+          <RestaurantCard
+            {...restaurant}
+            key={restaurant._id}
+            whereIsthisres="asf"
+          />
         ))}
       </div>
     </>
