f47ea1b768b17259a5f109c64fac791db958afb3
HarshilDale <harshil125.dale@gmail.com>
2021-10-05 20:44:21 +0530

    redux setup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import Store from "./Redux/store";
 
 // Import css files
 import "slick-carousel/slick/slick.css";
@@ -10,9 +12,11 @@ import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={Store}>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
