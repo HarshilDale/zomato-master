e9bc2e31d57ce9d4cfdb7cef4e35fbffba4b0214
HarshilDale <harshil125.dale@gmail.com>
2021-10-06 16:05:20 +0530

    add redirect

--- client/src/Components/RestaurantCard.jsx
+++ client/src/Components/RestaurantCard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { AiTwotoneStar } from "react-icons/ai";
 import { getImage } from "../Redux/Reducer/Image/Image.action";
@@ -18,45 +19,47 @@ const RestaurantCard = (props) => {
 
   return (
     <>
-      <div className="bg-white mb-4 p-4 w-full rounded-2xl transition duration-700 ease-in-out hover:shadow-lg md:w-1/2 lg:w-1/4">
-        <div className="w-full h-56 lg:h-64 relative">
-          <div className="absolute flex w-full bottom-4 items-end justify-between">
-            <div className="flex flex-col gap-2 items-start">
-              {props.isPro && (
-                <span className="bg-harshilred-400 text-white px-2 py-1 rounded text-sm">
-                  Pro extra 10% off
-                </span>
-              )}
-              {props.isOff && (
-                <span className="bg-blue-500 text-white px-2 py-1 rounded text-sm">
-                  ₹{`${props.isOff}`} OFF
-                </span>
-              )}
+      <Link to={`/restaurant/${props._id}`} className="w-full">
+        <div className="bg-white mb-4 p-4 w-full rounded-2xl transition duration-700 ease-in-out hover:shadow-lg md:w-1/2 lg:w-1/4">
+          <div className="w-full h-56 lg:h-64 relative">
+            <div className="absolute flex w-full bottom-4 items-end justify-between">
+              <div className="flex flex-col gap-2 items-start">
+                {props.isPro && (
+                  <span className="bg-harshilred-400 text-white px-2 py-1 rounded text-sm">
+                    Pro extra 10% off
+                  </span>
+                )}
+                {props.isOff && (
+                  <span className="bg-blue-500 text-white px-2 py-1 rounded text-sm">
+                    ₹{`${props.isOff}`} OFF
+                  </span>
+                )}
+              </div>
+              <span className="bg-white bg-opacity-75 p-1 rounded mr-3">
+                {props.durationOfdelivery} min
+              </span>
             </div>
-            <span className="bg-white bg-opacity-75 p-1 rounded mr-3">
-              {props.durationOfdelivery} min
-            </span>
+            <img
+              src={image.images.length && image.images[0].location}
+              alt="food"
+              className="w-full h-full rounded-2xl"
+            />
           </div>
-          <img
-            src={image.images.length && image.images[0].location}
-            alt="food"
-            className="w-full h-full rounded-2xl"
-          />
-        </div>
 
-        <div className="my-2 flex flex-col gap-2">
-          <div className="flex items-center justify-between">
-            <h4 className="text-xl font-medium">{props.name}</h4>
-            <span className="bg-green-700 text-white text-sm p-1 rounded flex items-center">
-              {props.restaurantReviewValue} <AiTwotoneStar />
-            </span>
-          </div>
-          <div className="flex items-center justify-between text-gray-500">
-            <p>{props.cuisine.join(", ")}</p>
-            <p>₹ {props.averageCost} for one</p>
+          <div className="my-2 flex flex-col gap-2">
+            <div className="flex items-center justify-between">
+              <h4 className="text-xl font-medium">{props.name}</h4>
+              <span className="bg-green-700 text-white text-sm p-1 rounded flex items-center">
+                {props.restaurantReviewValue} <AiTwotoneStar />
+              </span>
+            </div>
+            <div className="flex items-center justify-between text-gray-500">
+              <p>{props.cuisine.join(", ")}</p>
+              <p>₹ {props.averageCost} for one</p>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 };
---
+++ client/src/Page/Restaurant/Redirect.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
+
+const Redirect = () => {
+  const history = useHistory();
+  const { id } = useParams();
+  useEffect(() => {
+    history.push(`/restaurant/${id}/overview`);
+  });
+  return <></>;
+};
+
+export default Redirect;
